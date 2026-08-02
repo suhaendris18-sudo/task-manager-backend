6c627d6b62c38db32eab67fbfa65c693ebfdb204
New <suhaendris18@gmail.com>
2026-08-02 23:34:22 +0300

    commit

--- frontend/app.js
+++ frontend/app.js
@@ -1,68 +1,81 @@
 const API_URL = "https://task-manager-backend-71vt.onrender.com/api/tasks";
-const taskForm = document.getElementById('task-form');
-const titleInput = document.getElementById('title');
-const priorityInput = document.getElementById('priority');
-const taskList = document.getElementById('task-list');
+const taskForm = document.getElementById("task-form");
+const titleInput = document.getElementById("title");
+const priorityInput = document.getElementById("priority");
+const taskList = document.getElementById("task-list");
 async function fetchTasks() {
   try {
     const res = await fetch(API_URL);
     const tasks = await res.json();
     renderTasks(tasks);
   } catch (err) {
-    console.error('Error fetching tasks:', err);
+    console.error("Error fetching tasks:", err);
   }
 }
 function renderTasks(tasks) {
-  taskList.innerHTML = '';
-  tasks.forEach(task => {
-    const li = document.createElement('li');
+  taskList.innerHTML = "";
+  tasks.forEach((task) => {
+    const li = document.createElement("li");
     li.innerHTML = `
       <span>
         <strong>${task.title}</strong> (${task.priority})
-        <p>Status: ${task.completed ? "Completed" : "Not completed"}</p>
+        <p>${task.completed ? "Completed" : "Not completed"}</p>
       </span>
-      <button class="btn-delete" onclick="deleteTask(${task.id})">Delete</button>
-      <button class="task-btn complete-btn" onclick="completeTask(${task.id}, ${task.completed})">
-    ${task.completed ? "Undo" : "Complete"}
-</button>
+      <button class="btn-delete">Delete</button>
+      <button class="task-btn complete-btn">
+        ${task.completed ? "Undo" : "Complete"}
+      </button>
     `;
+
+    const completeBtn = li.querySelector(".complete-btn");
+    const deleteBtn = li.querySelector(".btn-delete");
+    completeBtn.addEventListener("click", () => {
+      completeTask(task.id, task.completed);
+    });
+    deleteBtn.addEventListener("click", () => {
+      deleteTask(task.id);
+    });
+
     taskList.appendChild(li);
   });
 }
-taskForm.addEventListener('submit', async (e) => {
+
+taskForm.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const newTask = {
     title: titleInput.value,
     priority: priorityInput.value,
-    completed: false
+    completed: false,
   };
   try {
     const res = await fetch(API_URL, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(newTask)
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newTask),
     });
     if (res.ok) {
-      titleInput.value = '';
-      fetchTasks(); 
+      titleInput.value = "";
+      fetchTasks();
     }
   } catch (err) {
-    console.error('Error adding task:', err);
+    console.error("Error adding task:", err);
   }
 });
+
 async function deleteTask(id) {
   try {
     const res = await fetch(`${API_URL}/${id}`, {
-      method: 'DELETE'
+      method: "DELETE",
     });
     if (res.ok) {
-      fetchTasks(); 
+      fetchTasks();
     }
   } catch (err) {
-    console.error('Error deleting task:', err);
+    console.error("Error deleting task:", err);
   }
 }
+
 async function completeTask(id, completed) {
   try {
     const response = await fetch(`${API_URL}/${id}`, {
@@ -70,7 +83,6 @@ async function completeTask(id, completed) {
       headers: {
         "Content-Type": "application/json",
       },
-
       body: JSON.stringify({
         completed: !completed,
       }),
